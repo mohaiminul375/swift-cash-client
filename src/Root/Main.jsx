1770e2d3254d353137d689b05911c2b31291b881
Mohaiminul375 <mohaiminul375@gmail.com>
2024-07-16 18:50:08 +0600

    create: create projects

---
+++ src/Root/Main.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+      <footer>
+        
+      </footer>
+    </>
+  );
+};
+
+export default Main;
