31872acfbb831edd813a59b3573e00ecaf67a9bb
Mohaiminul375 <mohaiminul375@gmail.com>
2024-07-17 17:45:51 +0600

    update: vefiy user and password in login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,17 @@
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import useAxiosPublic from "../hoooks/useAxiosPublic";
 
 const Login = () => {
-    // react hook form
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+  const axiosPublic = useAxiosPublic();
+  // react hook form
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (user_info) => {
+    console.log(user_info);
+    const { data } = await axiosPublic.post("/login", user_info);
+
+    console.log(data)
+  };
   return (
     <section className="min-h-[calc(100vh-90px)]">
       <div className="mt-20 border-2 border-[#e2136e] md:w-96 mx-auto rounded-md bg-[#e2136e] text-white p-5">
@@ -13,14 +20,14 @@ const Login = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control">
             <label className="label">
-              <span className="label-text text-white">Email</span>
+              <span className="label-text text-white">Phone Number</span>
             </label>
             <input
-              type="email"
-              placeholder="email"
+              type="number"
+              placeholder="number"
               className="input input-bordered text-black"
               required
-              {...register('email')}
+              {...register("phone_number")}
             />
           </div>
           <div className="form-control">
@@ -32,14 +39,20 @@ const Login = () => {
               placeholder="password"
               className="input input-bordered text-black"
               required
-              {...register('password')}
+              {...register("password")}
             />
           </div>
           <div>
-            <button className="text-center w-full mt-5 bg-white rounded-md py-1 text-[#e2136e] font-bold">Login</button>
+            <button className="text-center w-full mt-5 bg-white rounded-md py-1 text-[#e2136e] font-bold">
+              Login
+            </button>
           </div>
-        </form> 
-        <p className="mt-2 text-center text-lg">New here? <Link className="hover:underline" to='/register'>Please Register </Link>
+        </form>
+        <p className="mt-2 text-center text-lg">
+          New here?{" "}
+          <Link className="hover:underline" to="/register">
+            Please Register{" "}
+          </Link>
         </p>
       </div>
     </section>
